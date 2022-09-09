c0b31bc37acf296a1c266047885ea0e63f335150
Orion Oliveira <85138225+OrionOliveira@users.noreply.github.com>
2022-09-09 11:56:14 -0300

    Sistema de Login, Logout e Data Base
    Layout de Login quase pronto, sistema simples de data base, mas futuramente será por tabela, e um logout para sair da conta de usuário

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
+import json
 
 class random(BoxLayout):
     pass
@@ -13,28 +14,42 @@ class Manager(ScreenManager):
 class Menu(Screen):
     _telaprodutos = ObjectProperty(None)
 
-    def Signed(self):
-        if self.ids.signin_button.text == 'Logout':
-            self.ids.sigin_button.text = 'Signin'
-        self.ids.login_button.text = self.parent.ids.sgn.seller.text
-
     def removeProduct(self):
         self.ids.box_shop.remove_widget()
 
 class Signin(Screen):
     seller = ObjectProperty(None)
+    users_list = []
+
     def __init__(self, nome = '', idade = '', email = '', senha = '', cadastro = False, **kwargs):
         super().__init__(**kwargs)
+        self.loadData()
         self.a = NULL
         self.nome = nome
         self.cadastro = cadastro
 
     def signin(self):
-        if self.cadastro == False:
-            self.nome = str(self.seller.text)
-            self.cadastro = True
+        m = Menu()
+        m.__init__()
+        self.nome = str(self.seller.text)
+        if self.nome == '':
+            print('Vai dar não!')
+        elif self.nome in self.users_list:
+            print('Esse usuário já existe, deseja logar?')
+        else:
+            self.users_list.append(self.nome)
+            #self.cadastro = True
+            self.saveData()
             print('Cadastrado!')
-        return 
+            print(f"Lista de usuários: {self.users_list}")
+    
+    def saveData(self):
+        with open('users.json', 'w') as data:
+            json.dump(self.users_list, data)
+
+    def loadData(self):
+        with open('users.json', 'r') as data:
+            self.users_list = json.load(data)
         
 class Login(Screen):
     pass
